4147e1215e91ce9f64a6c888ae90f144a74e9a9a
EmersonGuerreroDEV <emersonguerrero.fr@academia.umb.edu.co>
2024-09-27 12:24:48 -0500

    setting environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,23 +6,33 @@ import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { jwtConstants } from './common/utils/constans';
 import { JwtModule } from '@nestjs/jwt';
-
+import { EnvConfiguration } from './config/configuration';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
       global: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '82.197.82.49',
-      port: 3306,
-      username: 'u878370420_undertake',
-      password: 'Berasflow21#',
-      database: 'u878370420_undertake',
-      entities: [User],
-      synchronize: true, // Solo para desarrollo
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port') || 3306, // Asegúrate de usar el puerto correcto
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.database'),
+        // Si usas una URL en lugar de los campos separados:
+        // url: configService.get<string>('database.url'),
+        entities: [], // Define tus entidades aquí
+        synchronize: true, // Solo para desarrollo, desactívalo en producción
+      }),
     }),
     TypeOrmModule.forFeature([User]),
   ],
---
+++ src/config/configuration.ts
@@ -0,0 +1,8 @@
+export const EnvConfiguration = () => ({
+  database: {
+    host: '82.197.82.49',
+    username: 'u878370420_undertake',
+    password: 'Berasflow21#',
+    database: 'u878370420_undertake',
+  },
+});
